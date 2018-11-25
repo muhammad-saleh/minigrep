bd5a0ade1a1a3a4f0f8f383a70123751c0f98bea
Mohamed Saleh <msaleh.eg87@gmail.com>
2018-11-25 03:14:42 +0200

    chore(*): using a struct with a constructor

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,24 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    let contents = fs::read_to_string(filename)
+    let config = Config::new(&args);
+    let contents = fs::read_to_string(config.filename)
         .expect("Error reading file");
 
     println!("With text:\n{}", contents);
 }
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
+}
+
