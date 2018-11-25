b6b544f539961a7f33e48fde7b1a2c338336fa60
Mohamed Saleh <msaleh.eg87@gmail.com>
2018-11-25 02:19:03 +0200

    chore(*): reading the file and print it's contents

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let query = &args[1];
+    let filename = &args[2];
+    let contents = fs::read_to_string(filename)
+        .expect("Error reading file");
+
+    println!("With text:\n{}", contents);
 }
